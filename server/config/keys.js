bde6ac84d62f5fd1beb95db2da921899ca0ee9db
Gopal8989 <59607382+Gopal8989@users.noreply.github.com>
2021-02-07 20:01:20 +0530

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://Santoshdb:sant@1439@mern-auth-ry0bq.mongodb.net/test?retryWrites=true&w=majority',
-  secretOrKey: 'secret'
-};
-//mongodb+srv://Santoshdb:<password>@mern-auth-ry0bq.mongodb.net/test?retryWrites=true&w=majority
